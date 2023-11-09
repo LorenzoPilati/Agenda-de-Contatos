6d7364118713798723f17265695849e3fe2b8e43
LorenzoPrograming <lm.pilati@gmail.com>
2023-11-09 10:48:20 -0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBz_vu46B9_BXUo13KFJA_RzsVJnrWm5I0",
+    authDomain: "appcontatos-696f7.firebaseapp.com",
+    projectId: "appcontatos-696f7",
+    storageBucket: "appcontatos-696f7.appspot.com",
+    messagingSenderId: "910609435205",
+    appId: "1:910609435205:web:20f419f7ed3cda2349312c",
+    measurementId: "G-T37MTSZTLV"  
+  }
+};
