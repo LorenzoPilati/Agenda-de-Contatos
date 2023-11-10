856d80454be803f32afbb6d7624e961a14df3cc3
LorenzoPrograming <lm.pilati@gmail.com>
2023-11-10 09:40:51 -0300

    storage

--- src/app/model/services/firebase.service.ts
+++ src/app/model/services/firebase.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import Contato from '../entities/Contato';
+import { finalize } from 'rxjs/operators';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,7 @@ import Contato from '../entities/Contato';
 export class FirebaseService {
   private PATH : string = 'contatos';
 
-  constructor(private firestore : AngularFirestore) { }
+  constructor(private firestore : AngularFirestore, private storage: AngularFireStorage) { }
 
   read(){
     return this.firestore.collection(this.PATH)
@@ -20,6 +22,11 @@ export class FirebaseService {
     .add({nome: contato.nome, telefone: contato.telefone});
   }
 
+  createWithAvatar(contato: Contato){
+    return this.firestore.collection(this.PATH)
+    .add({nome: contato.nome, telefone: contato.telefone, downloadURL: contato.downloadURL});
+  }
+
   update(contato: Contato, id: string){
     return this.firestore.collection(this.PATH).doc(id)
     .update({nome: contato.nome, telefone: contato.telefone});
@@ -29,5 +36,29 @@ export class FirebaseService {
     return this.firestore.collection(this.PATH)
     .doc(contato.id)
     .delete()
+  }
+
+  uploadImage(imagem: any, contato: Contato){
+    const file = imagem.item(0);
+    if(file.type.split('/')[0] != 'image'){
+      console.error('Tipo não Suportado!');
+      return;
+    }
+    const path = `images/${contato.nome}_${file.name}`;
+    const fileRef = this.storage.ref(path);
+    let task = this.storage.upload(path, file);
+    task.snapshotChanges().pipe(
+      finalize(() => {
+        let uploadFileURL = fileRef.getDownloadURL();
+        uploadFileURL.subscribe(resp => {
+          contato.downloadURL = resp;
+          if(!contato.id){
+            this.createWithAvatar(contato);
+          }else{
+            this.update(contato, contato.id);
+          }
+        })
+      })
+    ).subscribe();
   }
 }
